6bcd24473f037ff9431243dbbeb3f1a5d49c4cd5
eshirsch <eshirsch@hamilton.edu>
2026-09-12 15:40:59 -0400

    update

--- p-colorability.py
+++ p-colorability.py
@@ -1,4 +1,4 @@
-# @author Garrett Rhoads
+# @author Garrett Rhoads, Emma Hirsch
 # @file p-colorability.py
 # @date 9-7-26
 # @brief assign variables to each arc label all crossings positive or negative 
